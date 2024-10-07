7666babd42b3f189023c378d27df1f75d3466e0a
kal-kyokya <kalkyokya4@gmail.com>
2024-10-07 03:48:50 -0700

    Create the function generating a Pascal Triangle based on an integer input.

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 '0-pascal_triangle' creates a function that generate a Pascal triangle
 """
@@ -11,14 +11,46 @@ def pascal_triangle(n):
     Return:
         triangle: A list of list representing Pascal's triangle.
     """
-    # Declare the return variable 'triangle'
-    triangle = []
+    # Initialize a list of n element to be used as return value
+    triangle = [0 for i in range(n)]
 
     # Return an empty list if n is less than 0
-    if (n < 0) return []
+    if (n < 0): return []
 
     # Handle cases where input is 0
-    if (n == 0) return [1]
+    if (n == 0): return [1]
 
-    # Implement Pascal's Triangle logic
+    # Handle cases where input is 1
+    if (n == 1): return [1, 1]
+
+    # Implementing Pascal's Triangle logic:
+    # Initialize the first two rows
+    triangle[0] = [1]
+    triangle[1] = [1, 1]
+
+    # Keep track of the row being processed currently
+    current = 2
+
+    # Loop through all rows of the triangle
+    while (current < n):
+        size = current + 1
+        row = [0 for i in range(size)]
+        row[0] = row[-1] = 1
+
+        # Track the row element's indices
+        forward = 1
+        backward = -2
+
+        # Loop through the row
+        previous = triangle[current - 1]
+        while (forward <= size/2):
+            row[forward] = previous[forward - 1] + previous[forward]
+            row[backward] = row[forward]
+
+            forward += 1
+            backward -= 1
+
+        triangle[current] = row
+        current += 1
+
     return triangle
