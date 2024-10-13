cca8e98c84e85b659e83fd246e40754042d2820c
kal-kyokya <kalkyokya4@gmail.com>
2024-10-13 14:21:01 -0700

    Adhere to Python style enforced by the 'pycodestyle' linter.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-'0-lockboxes' holds a python script that solves a problem involving nested lists.
+'0-lockboxes' has a python script that solves a problem involving nested lists.
 """
+
+
 def canUnlockAll(boxes):
     """Determine whether keys to all boxes were acquired during search.
     Args:
@@ -10,22 +12,24 @@ def canUnlockAll(boxes):
         A boolean; True if all boxes were opened, False otherwise.
     """
     # Ensure that the input is a list of lists
-    if (type(boxes) != list or len(boxes) == 0): return (False)
+    if (type(boxes) != list or len(boxes) == 0):
+        return (False)
 
     # Store the number of available boxes
     boxes_no = len(boxes)
 
     # Handle single box case
-    if (boxes_no == 1): return (True)
+    if (boxes_no == 1):
+        return (True)
 
     # Ready a variable to hold the keys found in each box
     keys = set()
 
     # Handle Case of two boxes
     if (boxes_no == 2):
-        if (len(boxes[0]) == 0): return (False)
-        else:
-            return (True if (1 in boxes[0]) else False)
+        if (len(boxes[0]) == 0):
+            return (False)
+        return (True if (1 in boxes[0]) else False)
 
     # Case: More than two boxes
     visited = set()
@@ -37,14 +41,17 @@ def canUnlockAll(boxes):
         for key in boxes[visiting]:
             keys.add(key)
 
-        if (len(keys) == boxes_no): return (True)
-        if (len(keys) == (boxes_no - 1) and 0 not in keys): return (True)
+        if (len(keys) == boxes_no):
+            return (True)
+        if (len(keys) == (boxes_no - 1) and 0 not in keys):
+            return (True)
 
         visited.add(visiting)
         not_visited = list(filter(lambda x: x not in visited, boxes[visiting]))
         to_visit.extend(not_visited)
 
-        if (len(to_visit) == 0): return False
+        if (len(to_visit) == 0):
+            return False
 
         visiting = to_visit.pop(0)
 
