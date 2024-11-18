25895eb48eeafd9dee957ddb6117702386fccabd
kal-kyokya <kalkyokya4@gmail.com>
2024-11-18 20:37:31 +0000

    Display API generated names of star wars characters based on mobie ID.

--- 0x06-starwars_api/starwars_characters.js
+++ 0x06-starwars_api/starwars_characters.js
@@ -3,6 +3,11 @@ const util = require('util');
 const request = util.promisify(require('request'));
 const filmID = process.argv[2];
 
+if (process.argv.length < 3) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
 async function starwarsCharacters (filmId) {
   const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
   let response = await (await request(endpoint)).body;
