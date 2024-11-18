6e2f51817c0fbc7c3c5be2d8734f5cb8299558f4
kal-kyokya <kalkyokya4@gmail.com>
2024-11-18 20:31:38 +0000

    Display API generated names of star wars characters based on mobie ID.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,53 @@
 #!/usr/bin/node
-const util = require('util');
-const request = util.promisify(require('request'));
-const filmID = process.argv[2];
 
-async function starwarsCharacters (filmId) {
-  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-  let response = await (await request(endpoint)).body;
-  response = JSON.parse(response);
-  const characters = response.characters;
+const request = require('request');
 
-  for (let i = 0; i < characters.length; i++) {
-    const urlCharacter = characters[i];
-    let character = await (await request(urlCharacter)).body;
-    character = JSON.parse(character);
-    console.log(character.name);
-  }
+if (process.argv.length < 3) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
 }
 
-starwarsCharacters(filmID);
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Fetch movie details
+request(apiUrl, { json: true }, (err, res, body) => {
+  if (err) {
+    console.error('Error:', err);
+    return;
+  }
+  if (res.statusCode !== 200) {
+    console.error(`Error: Unable to fetch data (Status code: ${res.statusCode})`);
+    return;
+  }
+
+  // Ensure "characters" list exists in the response
+  if (!body.characters || !Array.isArray(body.characters)) {
+    console.error('Error: No character data found for this movie.');
+    return;
+  }
+
+  const characterUrls = body.characters;
+  const characterNames = new Array(characterUrls.length); // Placeholder for names
+  let completedRequests = 0;
+
+  characterUrls.forEach((url, index) => {
+    request(url, { json: true }, (charErr, charRes, charBody) => {
+      if (charErr) {
+        console.error('Error fetching character data:', charErr);
+      } else if (charRes.statusCode === 200) {
+        characterNames[index] = charBody.name; // Ensure order
+      } else {
+        console.error(`Error fetching character (Status Code:; ${charRes.statusCode})`);
+      }
+      completedRequests++;
+
+      // Print all character once all requests are completed
+      if (completedRequests === characterUrls.length) {
+        characterNames.forEach((name) => {
+          if (name) console.log(name);
+        });
+      }
+    });
+  });
+});
