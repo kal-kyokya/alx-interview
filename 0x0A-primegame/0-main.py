47471e09667a9409bc67a74bc4e6bb7f216e0963
kal-kyokya <kalkyokya4@gmail.com>
2024-12-13 00:04:30 -0800

    Run a test file utilizing the prime game script.

--- 0x0A-primegame/0-main.py
+++ 0x0A-primegame/0-main.py
@@ -4,3 +4,7 @@ isWinner = __import__('0-prime_game').isWinner
 
 
 print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+
+print("Winner: {}".format(isWinner(50, [92, 15, 31, 74, 53])))
+
+print("Winner: {}".format(isWinner(13, [11, 57, 19, 41, 37])))
